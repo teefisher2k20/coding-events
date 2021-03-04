e9d6b90ba227ef68075064824954d588ce498ef2
Terrance Fisher <teefisher314@gmail.com>
2021-03-03 18:37:24 -0600

    12.3.5. Delete an Event

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -40,4 +40,22 @@ public class EventController {
         return "redirect:";
     }
 
-}
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Event");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
+            return "redirect:";
+        }
+    }
